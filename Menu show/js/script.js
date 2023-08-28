23f05bd0e270a4164cbea66196a4f86ebe0a6721
Astghik <99022244+AstghikDolmazyan@users.noreply.github.com>
2023-08-28 23:28:47 +0400

    thord commit
    3 folders

---
+++ Menu show/js/script.js
@@ -0,0 +1,17 @@
+function menu2(){
+    document.getElementById('manual').style.display='block';	
+}
+
+function closemenu(){
+	document.getElementById("manual").style.display="none";
+}
+
+function contact2(){
+    document.getElementById("constanto").style.display="block";	
+}
+
+function closemenu2(){
+	document.getElementById("constanto").style.display="none";
+}
+
+
